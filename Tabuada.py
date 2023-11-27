0613e885df872fcca41b1eb81a0e08c83d8fd3b7
LeonardoKelmy <leonardosenaijacob@gmail.com>
2023-11-27 15:42:46 -0300

    Commit inicial tabuada

---
+++ Tabuada.py
@@ -0,0 +1,36 @@
+#tabuada
+
+#apresentação
+# print('\n\t\t\t -- tabuada --\n')
+
+#neste projeto você conseguirá obter a tabuada dos números de 1 a 10
+
+#entradas
+
+n1 = int(input('informe o número que você quer multiplicar: '))
+
+#processamento
+mult1  = n1*1 #aqui será todo número multiplicado por 1
+mult2  = n1*2 #aqui será todo número multiplicado por 2
+mult3  = n1*3 #aqui será todo número multiplicado por 3
+mult4  = n1*4 #aqui será todo número multiplicado por 4
+mult5  = n1*5 #aqui será todo número multiplicado por 5
+mult6  = n1*6 #aqui será todo número multiplicado por 6
+mult7  = n1*7 #aqui será todo número multiplicado por 7
+mult8  = n1*8 #aqui será todo número multiplicado por 8
+mult9  = n1*9 #aqui será todo número multiplicado por 9
+mult10  = n1*10 #aqui será todo número multiplicado por 10
+
+#saida
+print(f'{n1} * 1 = {mult1}')
+print(f'{n1} * 2 = {mult2}')
+print(f'{n1} * 3 = {mult3}')
+print(f'{n1} * 4 = {mult4}')
+print(f'{n1} * 5 = {mult5}')
+print(f'{n1} * 6 = {mult6}')
+print(f'{n1} * 7 = {mult7}')
+print(f'{n1} * 8 = {mult8}')
+print(f'{n1} * 9 = {mult9}')
+print(f'{n1} * 10 = {mult10}')
+
+
